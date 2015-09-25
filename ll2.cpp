25440ccc27e3c9086ced1c7712a9c3c1f0e27b3d
Hector Rincon <hecerinc@hotmail.com>
2015-09-24 20:57:38 -0500

    Added the real linked list that uses pointers. Erased the original one.

--- ll2.cpp
+++ ll2.cpp
@@ -12,21 +12,17 @@ public:
 
 
 	//Agrega un elemento al final. de la lista
-	void push(Node n){
+	void push(Node* n){
 		/*
 			Aqui TIENES que pasarlo por referencia (la direccion/un apuntador)
 			en vez de un parametro por valor porque estas asignando la direccion, pero el Nodo n (que es una copia que se crea del nodo que pasaste en el main) se destruye cuando sale de la funcion push... entonces ya no estarias apuntando a nada.
 		*/
-		Node * mio = new Node;
-		mio->data = n.data;
-		mio->next = NULL;
-		n.next = NULL;
+		n->next = NULL;
 		Node *last = this->getLast();
 		if(last != NULL)
-			last->next = mio;
+			last->next = n;
 		else
-			head = mio;
-		cout << head->data << endl; //12
+			head = n;
 	}
 
 
@@ -63,16 +59,11 @@ public:
 
 	void show(){
 		Node *ptr = this->head;
-		if(head == NULL)
-			cout << "soy NULL" << endl;
-		else
-			cout << "not null" << endl;
-		cout << this->head->data << endl;
 		cout << "The List is ";
-		// while(ptr!=NULL){
-		// 	cout << ptr->data << " -> ";
-		// 	ptr = ptr->next;
-		// }
+		while(ptr!=NULL){
+			cout << ptr->data << " -> ";
+			ptr = ptr->next;
+		}
 		cout << endl;
 	}
 	Node * getHead(){return head;};
@@ -82,8 +73,8 @@ private:
 };
 int main(){
 	LinkedList list;
-	Node a;
-	a.data = 12;
+	Node* a = new Node;
+	a->data = 12;
 	cout << list.isEmpty() << endl;
 	list.push(a);
 	cout << (list.getHead())->data << endl;
